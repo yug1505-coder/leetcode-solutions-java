b8d3bc377cd52cf31fffc4ace55f5734b0af5d07
Yug Babbar <yugbabbar@gmail.com>
2026-07-17 12:10:11 +0530

    Time: 0 ms (100%), Space: 43.5 MB (60.98%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,6 +1,38 @@
 class Solution {
     public void sortColors(int[] nums) {
-        Arrays.sort(nums);
+        int n = nums.length;
+        int countZero = 0;
+        int countOne = 0;
+        int countTwo = 0;
+        for(int i=0; i<n; i++){ //loop iterate to count number of 0 ,1 and 2.
+            if(nums[i]==0){
+                countZero++;
+            }
+            else if(nums[i]==1){
+                countOne++;
+            }
+            else{
+                countTwo++;
+            }
+        }
+        int k = 0; //now filling numbers as per condition 
+        while(countZero>0){
+            nums[k]=0;
+            k++;
+            countZero--;
+        }
+        
+        while(countOne>0){
+            nums[k]=1;
+            k++;
+            countOne--;
+        }
+        
+        while(countTwo>0){
+            nums[k]=2;
+            k++;
+            countTwo--;
+        }
         
     }
 }
