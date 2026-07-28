6402fdf6fbfd91c7d7487fefa6ab4dcdab81241c
Yug Babbar <yugbabbar@gmail.com>
2026-07-28 22:26:47 +0530

    Time: 2 ms (99.32%), Space: 44.5 MB (55.73%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,28 +1,33 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        int left = 0;
-        int right = s.length()-1;
-        while(left<right){
-            char l = s.charAt(left);
-            char r = s.charAt(right);
-            if(!isAlphaNum(l)){ //left not alphanum ,so skip
-                left++;
-            }
-            else if(!isAlphaNum(r)){//right not alphanum, so skip 
-                right--;
+       char[] arr = s.toCharArray();
+       int left = 0;
+       int right = arr.length-1;
+       while(left<right){
+        if(!isAlphaNumeric(arr[left])){
+            left++; 
+        }
+        else if(!isAlphaNumeric(arr[right])){
+            right--;
+        }
+        else{
+            if(Character.toLowerCase(arr[left]) != Character.toLowerCase(arr[right])){
+                return false;
             }
             else{
-                if(Character.toLowerCase(l)!=Character.toLowerCase(r)){
-                    return false;
-                }
-                //if character matching we move left and right pointer both together!
                 left++;
                 right--;
             }
         }
-        return true;
+       }
+       return true;
     }
-    private boolean isAlphaNum(char c){
+
+ 
+
+    public boolean isAlphaNumeric(char c){
         return ((c>='a' && c<='z') || (c>='A' && c<='Z') || (c>='0' && c<='9'));
+
     }
+
 }
