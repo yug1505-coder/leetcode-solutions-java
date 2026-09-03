fedd2a7f45a38d5d8d0a11f444c30481f3265fdd
Yug Babbar <yugbabbar@gmail.com>
2026-09-03 13:36:00 +0530

    Time: 0 ms (100%), Space: 48.3 MB (55.44%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.java
+++ 0344-reverse-string/0344-reverse-string.java
@@ -1,13 +1,17 @@
 class Solution {
     public void reverseString(char[] s) {
-        int left = 0;
-        int right = s.length-1;
+        int n = s.length;
+        int left = 0, right = n-1;
         while(left<right){
             char temp = s[left];
             s[left] = s[right];
             s[right] = temp;
             left++;
             right--;
-        } 
+        }
+        
     }
 }
+// TC = O(n/2) because left aur right each iteration mein ek step andar aate hain.
+// But Big-O mein constant factor ignore hota hai, so O(n/2) = O(n).
+// SC = O(1) because sirf temp, left, right, etc. constant extra variables hain.
