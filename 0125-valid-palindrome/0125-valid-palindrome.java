a8c431f9cbeb1c4fe9ab6600d6a82eee71aae6dd
Yug Babbar <yugbabbar@gmail.com>
2026-09-03 15:34:05 +0530

    Time: 2 ms (99.29%), Space: 44.6 MB (47.42%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,17 +1,16 @@
 class Solution {
     public boolean isPalindrome(String s) {
-       char[] arr = s.toCharArray();
-       int left = 0;
-       int right = arr.length-1;
-       while(left<right){
-        if(!isAlphaNumeric(arr[left])){
-            left++; 
-        }
-        else if(!isAlphaNumeric(arr[right])){
-            right--;
-        }
-        else{
-            if(Character.toLowerCase(arr[left]) != Character.toLowerCase(arr[right])){
+        char[] arr = s.toCharArray();
+        int left = 0;
+        int right = arr.length - 1;
+        while (left < right) {
+            while(left<right && !isAlphaNumeric(arr[left])) {
+                left++;
+            }
+            while(left<right && !isAlphaNumeric(arr[right])) {
+                right--;
+            }
+            if(Character.toLowerCase(arr[left])!= Character.toLowerCase(arr[right])){
                 return false;
             }
             else{
@@ -19,15 +18,19 @@ class Solution {
                 right--;
             }
         }
-       }
-       return true;
-    }
 
- 
 
-    public boolean isAlphaNumeric(char c){
-        return ((c>='a' && c<='z') || (c>='A' && c<='Z') || (c>='0' && c<='9'));
+        return true;
 
     }
 
+    public boolean isAlphaNumeric(char c) {
+        if ((c >= '0' && c <= '9') || (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
+// TC: You can think of the two-pointer comparison as roughly O(n/2), because pointers move toward each other. But Big-O drops the constant 1/2, so we write O(n).
+// SC: O(n) because s.toCharArray() creates a new char[] containing the characters.
