c79cb273af4e64d85dfe04bbece59c4e5b02b5ae
Yug Babbar <yugbabbar@gmail.com>
2026-09-04 17:47:53 +0530

    Time: 0 ms (100%), Space: 43.4 MB (59.12%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,29 +1,28 @@
 class Solution {
     public void sortColors(int[] nums) {
         int n = nums.length;
-        int low = 0;
-        int k = 0; //scanning pointer
-        int high =  n-1;
-        while(k<=high){ // equal is considered bcz we dont which element high pointer is holding so we have to consider that case also.
-
-            if(nums[k]==0){
-                int temp  = nums[low];
-                nums[low] = nums[k];
+        int left = 0, right = n - 1, k = 0;
+        while (k<=right) {
+            // equal is considered bcz we dont which element high pointer is holding so we have to consider that case also.
+            if (nums[k] == 0) {
+                int temp = nums[left];
+                nums[left] = nums[k];
                 nums[k] = temp;
-                low++;
+                left++;
                 k++;
             }
             else if(nums[k]==1){
-                k++;
+                k++; 
             }
-            else{ //nums[k] == 2 case
-            int temp = nums[high];
-            nums[high] = nums[k];
-            nums[k] = temp;
-            high--;
+            else{
+                int temp = nums[right];
+                nums[right]= nums[k];
+                nums[k] = temp;
+                right--;
             }
         }
 
-        
     }
 }
+// TC: O(n)
+// SC: O(1)
