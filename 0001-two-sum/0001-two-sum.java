ae4a675cb9a03312cbefde2e59258bdff39d20c4
Yug Babbar <yugbabbar@gmail.com>
2026-09-05 08:22:32 +0530

    Time: 2 ms (99.36%), Space: 46.9 MB (74.26%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,16 +1,20 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         int n = nums.length;
-        HashMap<Integer,Integer> map = new HashMap<>();
-        for(int i=0; i<n; i++){
-         int x = target - nums[i];
-         if(map.containsKey(x)){
-            return new int[]{map.get(x),i};
-         }
-         else{
-         map.put(nums[i],i);
-        }
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            int ans = target - nums[i];
+            if (map.containsKey(ans)) {
+                return new int[] { map.get(ans), i };
+            } else {
+                map.put(nums[i], i);
+            }
         }
         return new int[]{};
     }
 }
+// Time: O(n)
+// You traverse the array once, and HashMap lookup/insertion is O(1) average.
+
+// Space: O(n)
+// In the worst case, you may store almost every element in the HashMap
