4d8c6b7e60c8f82079caeb3f5a9ba1acbd1d7946
Yug Babbar <yugbabbar@gmail.com>
2026-09-12 16:05:31 +0530

    Time: 30 ms (87.29%), Space: 59.2 MB (45.2%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -3,41 +3,43 @@ class Solution {
         int n = nums.length;
         List<List<Integer>> ans = new ArrayList<>();
         Arrays.sort(nums);
-        for(int i=0;i<n-2; i++){
-            if(i>0 && nums[i]==nums[i-1]){
+        for(int i=0; i<n-2; i++){
+            if(i>0 && nums[i]==nums[i-1]){ //same element again found so skipp 
                 continue;
             }
-            int left = i+1;
-            int right = n-1;
+            if(nums[i]>0){ // sum can't b greater than zero ever !
+                break;
+            }
+            int left = i+1, right = n-1;
             while(left<right){
-                int sum = nums[i]+nums[left]+nums[right];
-                if(sum==0){
-                    ans.add(Arrays.asList(nums[i],nums[left],nums[right]));
-                    left++;
-                    right--;
-                    while(left<right && nums[left]==nums[left-1]){
-                        left++;
-                    }
-                    while(left<right && nums[right]==nums[right+1]){
-                        right--;
-                    }
-                }
-                
-                else if(sum<0){
+            int sum = nums[i]+nums[left]+nums[right];
+            if(sum==0){
+                ans.add(Arrays.asList(nums[i],nums[left],nums[right]));
+                //initially first moved the pointers...
+                left++;
+                right--;
+
+                //conditons for duplicate elements when left and right pointer moved and new element same as previous one..
+
+                while(left<right && nums[left]==nums[left-1]){
                     left++;
                 }
-                else{
+                while(left<right && nums[right]==nums[right+1]){
                     right--;
                 }
 
             }
-
+            else if(sum<0){
+                left++;
+            }
+            else{
+                right--;
+            }
+            }
         }
         return ans;
-        
     }
 }
-// Sorting → O(n log n), Two-pointer loops → O(n²)
-// ans output → O(k), Auxiliary space → O(1)
 
-// TC = O(n²), SC = O(k)
+// total time - O(nlogn) for sorting + O(n square) == O(n square) !
+// total space  - if we exclude the memory used by the output arraylist (which is required by the problem), the Auxiliary Space is O(1).
