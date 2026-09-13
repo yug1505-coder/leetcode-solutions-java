9afadc4df1a99f43fc5703618e564598b0289d51
Yug Babbar <yugbabbar@gmail.com>
2026-09-13 23:42:08 +0530

    Time: 2 ms (91.7%), Space: 47.7 MB (76.07%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,18 +1,19 @@
 class Solution {
     public void moveZeroes(int[] nums) {
         int n = nums.length;
-        int k = 0;
-        int i = 0;
-   while(i<n){
+        int k = 0, i=0;
+        while(i<n){
             if(nums[i]!=0){
+                if(i!=k){ //avoiding unnecessary steps when zeroes are not in array.
                 int temp = nums[k];
-                nums[k] = nums[i];
-                nums[i]= temp;
+                nums[k]= nums[i];
+                nums[i] = temp;
+                }
                 k++;
-
             }
             i++;
-   }
+        }
+        
     }
 }
 // Time Complexity: O(n) — i array ko exactly once traverse karta hai. k bhi maximum n times move karta hai, so overall linear.
